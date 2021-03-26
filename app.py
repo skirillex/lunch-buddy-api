33d77037883b45f0e00a2d9fce0ebf74d1443930
Kirill <kirillmasychev@outlook.com>
2021-03-26 11:01:36 -0400

    added versioning

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+base_route = '/api/v1/'
 
-@app.route('/')
+@app.route(base_route)
 def hello():
     return "Hello World!"
+
 
 if __name__ == '__main__':
     app.run()
