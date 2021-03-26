876bae233647ae480726d12f4e98c922e8fa631a
Krishna Gaurav <krishnagaurav@C02YD3YPJGH5.local>
2021-03-26 13:54:47 -0400

    Added the end point for confirmation page

--- app.py
+++ app.py
@@ -9,13 +9,20 @@ records = db.register
 
 
 @app.route("/")
-def hello():
+def login():
     email = request.form.get("email")
     user = request.form.get("user")
     # email_found = records.find_one({"email": email})
-    user_input = {'name': user, 'email': email}
+    user_input = {'name': user, 'email': email, 'confirmation':False}
     records.insert_one(user_input)
     return user_input
+
+@app.route("/confirmation")
+def confirmation():
+    email = request.form.get("email")
+    confirmation = request.form.get("input")
+    if confirmation:
+        records.update_one({'email': email}, {'confirmation': True}),
 
 if __name__ == '__main__':
     app.run()
