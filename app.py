fa816227970b3d8475ddfd91f1f3597a1c50374e
Kirill <kirillmasychev@outlook.com>
2021-03-26 12:58:27 -0400

    some DBwork

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from pymongo import MongoClient
+
+client = MongoClient(host="localhost", port=27017)
+db = client["lunch-buddy"]
+user = db.user
+
 app = Flask(__name__)
 
 base_route = '/api/v1/'
 
 @app.route(base_route)
 def hello():
-    return "Hello World!"
+    return user.find()
 
 
 if __name__ == '__main__':
